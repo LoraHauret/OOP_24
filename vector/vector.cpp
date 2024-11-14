6c0c82d2b9d4fe7f9bd6c3f9050b8b88de15f3e2
LoraHauret <117381884+LoraHauret@users.noreply.github.com>
2024-11-14 16:34:42 +0200

    class Vector

---
+++ vector/vector.cpp
@@ -0,0 +1,402 @@
+﻿
+#pragma once
+#include "vector.h"
+
+ostream& operator<<(ostream& os, Vector& v)
+{
+    if (v.IsEmpty())
+    {
+        os << "Vector is empty.\n";
+        return os;
+    }
+
+    for (int i = 0; i < v.size; i++)
+    {
+        os << v.data[i] << " ";
+    }
+    os << "\n";
+
+    return os;
+}
+
+// Insert (вставка одного элемента в массив по указанному индексу, с проверкой на выход за пределы массива)
+bool Vector::Insert(int index, int value, int countinsert)//куда вставлять, что вставлять, сколько раз
+{     
+
+    if (index < 0 || index > size || countinsert == 0)
+        return false;
+
+    if (index == size)                        // если индекс вставки == количесву элементов, то вставляю в конец массива
+    {
+        for(int i = 0; i < countinsert; i++)
+            PushBack(value);
+        return true;
+    }
+        
+
+    EnsureCapacity(countinsert);
+    for (int i = size + countinsert-1; i > index; i--)
+        data[i] = data[i - countinsert];
+
+    size += countinsert;
+
+    while (countinsert)
+    {
+        data[index] = value;
+        --countinsert;
+        ++index;
+    }
+       
+    
+    //cout << *this;
+    return true;
+}
+
+
+//// (удаление одного элемента по указанному индексу. Должна быть проверка на допустимость индекса)
+bool Vector::RemoveAt(int index, int countdelete)
+{
+    if (IsEmpty())
+        return false;
+
+    if (index < 0 || index >= size || countdelete == 0)
+        return false;
+
+    if (countdelete >= size - index)   // если пользователь хочет удалить больше элементов, нежели находится правее от указанного индекса, то просто "удаляю" все элементы до конца массива
+    {
+        size = index;
+        return true;
+    }
+
+    for (int i = index + countdelete; i < size; i++)
+    {
+        data[index] = data[i];
+        ++index;
+    }      
+
+    size -= countdelete;
+    
+    return true;
+}
+
+//// RemoveByValue (удаление одного или всех элементов, значение которых совпадает со значением переданного параметра)
+bool Vector::RemoveByValue(int value, bool remAll)
+{
+    if(IsEmpty())
+        return false;
+    for (int i = 0; i < size; i++)
+    {
+        if (data[i] == value)
+        {
+            RemoveAt(i);
+            --i; // поскольку элемент удален 
+            if (remAll)
+                continue;
+            else
+                return true;
+        }
+            
+    }
+    return false; // элемент не найден
+}
+
+////  (удаление элемента по индексу 0 из массива)
+bool Vector::PopFront()
+{
+    return RemoveAt(0);
+}
+
+//  (удаление последнего элемента из массива)
+bool Vector::PopBack()
+{
+    return RemoveAt(size-1);
+}
+
+  //(метод подгоняет значение capacity под size, естественно с перевыделением памяти)
+bool Vector::TrimToSize()
+{ 
+    if (IsEmpty() || size == capacity)
+        return false;
+
+    int* temp = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        temp[i] = data[i];
+    }
+    delete[] data;
+    data = temp;
+    capacity = size;
+    return true;
+}
+
+
+//  (линейный поиск слева направо первого вхождения в массив указанного значения. В результате работы вернуть индекс найденного элемента, а eсли ничего не найдено, вернуть -1)
+int Vector::IndexOf(int value)
+{
+    if (IsEmpty())
+        return -1;
+    for (int i = 0; i < size; i++)
+    {
+        if (data[i] == value)
+            return i;
+    }
+    return -1;
+}
+
+//LastIndexOf(линейный поиск справа налево вхождения в массив указанного значения.В результате работы вернуть индекс найденного элемента, а eсли ничего не найдено, вернуть - 1)
+int Vector::LastIndexOf(int value)
+{
+    if (IsEmpty())
+        return -1;
+    
+    for (int i = size - 1; i >=0 ; i--)
+    {
+        if (data[i] == value)
+            return i;
+    }
+    return -1;
+}
+
+
+//  (изменение порядка следования элементов в массиве на противоположный)
+bool Vector::Reverse()
+{
+    if (IsEmpty())
+        return false;
+    for (int i = 0, j = size - 1; i < size / 2; i++, j--)
+    {
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+    }
+    return true;
+}
+
+//SortAsc (быстрая сортировка значений элементов массива по возрастанию)
+bool Vector::SortAsc()
+{
+    if (IsEmpty())
+        return false;
+
+    for (int i = size-1; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (data[j] > data[j + 1])
+            {
+                int temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+            }
+        }     
+    }
+    return true;
+}
+
+//  SortDesc (быстрая сортировка значений элементов массива по убыванию)
+bool Vector::SortDesc()
+{
+    if (IsEmpty())
+        return false;
+
+    for (int i = size - 1; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (data[j] <  data[j + 1])
+            {
+                int temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+            }
+        }
+    }
+    return true;
+}
+
+bool Vector::Shuffle()
+{
+    if (IsEmpty())
+        return false;
+    for (int i = 0; i < 100; i++)
+    {
+        int ind1 = rand() % size;
+        int ind2 = rand() % size;
+        if (ind1 == ind2)
+            continue;
+        //cout << ind1 << " " << ind2 << endl;
+        int temp = data[ind1];
+        data[ind1] = data[ind2];
+        data[ind2] = temp;        
+    }
+    return true;
+}
+
+// (заполнение массива случайными значениями)
+bool Vector::RandomFill()
+{
+    if (!IsEmpty())
+    {
+        for (int i = 0; i < size; i++)
+        {
+            data[i] = rand() % 100;
+        }
+        return true;
+    } 
+    return false;
+}
+
+//  (в качестве параметра передаётся константную ссылку на другой вектор. Метод сравнивает массивы не только по количеству элементов, но и по их содержимому)
+bool Vector::Equals(const Vector& other)
+{
+    return (*this == other);
+}
+
+// GetElementAt (возврат копии элемента массива по указанному индексу, с проверкой на выход за пределы массива)
+int Vector::GetElementAt(int index)
+{
+    if (IsEmpty())
+        throw ("вектор пуст!");
+    if (index < 0 || index >= size)
+        throw out_of_range("индекс за пределами допустимых значений!");
+    return data[index];
+}
+
+//  (метод создаёт точную копию вектора).
+
+Vector*& Vector::Clone()
+{
+    Vector* ret = new Vector(size);
+    if (!IsEmpty())
+    {
+        for(int i = 0; i < size; i++)
+        {
+            ret->PushBack(data[i]);
+        }
+    }
+    return ret;
+}
+
+Vector::Vector(const Vector& other)
+{
+    cout << "\n\n ********* КОНСТРУКТОР  КОПИРОВАНИЯ ДЛЯ " << this << " **********\n\n";
+   
+
+    if (other.size != 0)
+    {
+        this->capacity = other.capacity;
+        this->size = other.size;        
+        data = new int[capacity];
+        //копирую один блок памяти в другой
+        // memcpy(data, other.data, sizeof(int) * size);
+        // или вот так
+        for (int i = 0; i < size; i++)
+           data[i] = other.data[i];
+    }
+    else
+    {
+        Vector();
+        
+    }        
+}
+
+// перегрузка операции =
+
+Vector& Vector::operator=(const Vector& other)
+{
+    if (*this != other)
+    {
+        Clear();
+        EnsureCapacity(other.size);
+
+        //копирую один блок памяти в другой
+        //for (int i = 0; i < other.size; i++)
+        //    this->PushBack(other.data[i]);        
+        // или так
+        // memcpy(data, other.data, sizeof(int) * size);
+        // или вот так
+        for (int i = 0; i < other.size; i++)
+            data[i] = other.data[i];
+
+        this->size = other.size;
+    }
+    
+    return *this;    //возвращаю объект, чтобы можно было присваивать векторы друг другу в цепочке присваивания
+}
+
+// перегрузка операции [ ]
+int& Vector::operator[](int index)
+{
+    if (IsEmpty())
+        throw ("вектор пуст!");
+    if (index < 0 || index >= size)
+        throw out_of_range("индекс за пределами допустимых значений!");
+    return data[index];
+}
+
+
+//// перегрузка операции ==
+bool Vector::operator==(const Vector& other)
+{
+    if (size != other.size)
+        return false;
+    for (int i = 0; i < size; i++)
+    {
+        if (data[i] != other.data[i])
+            return false;
+    }
+    return true;
+}
+
+bool Vector::operator!=(const Vector& other)
+{
+    return !(*this == other);
+}
+
+bool Vector::operator< (const  Vector& other)
+{
+    return size < other.size;
+}
+bool Vector::operator>(const   Vector& other)
+{
+    return size > other.size;
+}
+bool Vector::operator<= (const Vector& other)
+{
+    return size <= other.size;
+}
+bool Vector::operator>=(const  Vector& other)
+{
+    return size >= other.size;
+}
+
+void Vector::IncreaseCapacity()
+{
+    unsigned int temp_capacity = size + 10;
+    int* temp = new int[temp_capacity];
+    for (int i = 0; i < size; i++)
+        temp[i] = data[i];
+    delete data;
+    data = temp;
+    capacity = temp_capacity;
+}
+void Vector::IncreaseCapacity(int min_incr)
+{
+    unsigned int temp_capacity = size + min_incr + 10;
+    int* temp = new int[temp_capacity];
+    for (int i = 0; i < size; i++)
+        temp[i] = data[i];
+    delete[] data;
+    data = temp;
+    capacity = temp_capacity;
+}
+void Vector::EnsureCapacity()
+{
+    if (size == capacity)
+        IncreaseCapacity();
+}
+void Vector::EnsureCapacity(int incr)
+{
+    if (size + incr >= capacity)
+        IncreaseCapacity(incr);
+}
---
+++ vector/vector.h
@@ -0,0 +1,152 @@
+﻿#pragma once
+#include <iostream>
+#include <random>
+using namespace std;
+/*
+- Insert (вставка одного элемента в массив по указанному индексу, с проверкой на выход за пределы массива)
+- RemoveAt (удаление одного элемента по указанному индексу. Должна быть проверка на допустимость индекса)
+- RemoveByValue (удаление одного или всех элементов, значение которых совпадает со значением переданного параметра)
+- PopFront (удаление элемента по индексу 0 из массива)
+- PopBack (удаление последнего элемента из массива)
+- TrimToSize (метод подгоняет значение capacity под size, естественно с перевыделением памяти)
+- IndexOf (линейный поиск слева направо первого вхождения в массив указанного значения. В результате работы вернуть индекс найденного элемента, а eсли ничего не найдено, вернуть -1)
+- LastIndexOf (линейный поиск справа налево вхождения в массив указанного значения. В результате работы вернуть индекс найденного элемента, а eсли ничего не найдено, вернуть -1)
+- Reverse (изменение порядка следования элементов в массиве на противоположный)
+- SortAsc (быстрая сортировка значений элементов массива по возрастанию)
+- SortDesc (быстрая сортировка значений элементов массива по убыванию)
+- Shuffle (случайное перемешивание элементов массива)
+- RandomFill (заполнение массива случайными значениями)
+- Equals (в качестве параметра передаётся константную ссылку на другой вектор. Метод сравнивает массивы не только по количеству элементов, но и по их содержимому)
+- GetElementAt (возврат копии элемента массива по указанному индексу, с проверкой на выход за пределы массива)
+- Clone (метод создаёт точную копию вектора).
+- конструктор копирования
+- перегрузка операции =
+- перегрузка операции [ ]
+- перегрузка операции ==
+- перегрузка >>  и <<
+*/
+
+class Vector // Vector
+{
+    unsigned int size = 0; // количество действительно присутствующих элементов в контейнере
+    unsigned int capacity = 10; // ёмкость (вместительность, запас памяти)
+    int* data = nullptr; // указатель на динамический массив целых чисел
+
+    void IncreaseCapacity();
+    void IncreaseCapacity(int);
+    void EnsureCapacity();
+    void EnsureCapacity(int);
+public:
+    Vector() : Vector(10)
+    {
+	// cout << "C-TOR WITHOUT PARAMS!\n";
+    }
+
+    Vector(unsigned int capacity)
+    {
+	if (capacity < 10)
+	{
+	    capacity = 10;
+	}
+	this->capacity = capacity;
+	data = new int[capacity];
+	// cout << "C-TOR WITH PARAMS!\n";
+	cout << "\n\n ********* КОНСТРУКТОР ДЛЯ " << this << " **********\n\n";
+    }
+
+    ~Vector()
+    {
+	cout << "\n\n ********* ДЕСТРУКТОР ДЛЯ " << this << " **********\n\n";	
+	// cout << "DESTRUCTOR!\n";
+	if (data != nullptr)
+	{
+	    delete[] data;
+	    data = nullptr;
+	}
+    }
+
+    void PushBack(int value)
+    {
+	EnsureCapacity(); // проверка, хватит ли места для нового элемента
+	data[size] = value;
+	size++;
+    }
+
+    void PushFront(int value)
+    {
+	EnsureCapacity();
+	for (int i = size; i > 0; i--) // i = 1
+	{
+	    data[i] = data[i - 1]; // data[1] = data[0]
+	}
+	data[0] = value;
+	size++;
+    }
+
+    void Clear()
+    {
+	size = 0;
+    }
+
+    bool IsEmpty() const
+    {
+	return size == 0;
+    }
+
+    void Print() const
+    {
+	if (IsEmpty())
+	{
+	    cout << "Vector is empty.\n";
+	    return;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+	    cout << data[i] << " ";
+	}
+	cout << "\n";
+    }
+
+    // ...остальные методы...
+    bool Insert(int index, int n, int countinsert = 1);
+    bool RemoveAt(int index, int countdelete = 1);
+    bool RemoveByValue(int value, bool remOne = true);
+    bool PopFront();
+    bool PopBack();
+    bool TrimToSize();
+    int IndexOf(int value);
+    int LastIndexOf(int value);
+    bool Reverse();
+    bool SortAsc(); 
+    bool SortDesc();
+    bool Shuffle();
+    bool RandomFill();
+    bool Equals(const Vector& other);
+    int GetElementAt(int);
+    Vector*& Clone();
+    Vector(const Vector&);
+    Vector& operator=(const Vector&);
+ //  /* void testCopyConstructor(Vector v)
+ //   {	v.Print();
+	//cout << "testing" << endl;
+ //   }*/
+    int& operator[](int index);
+    int GetSize()
+    {
+	return size;
+    }
+    int GetCapacity()
+    {
+	return capacity;
+    }
+    bool operator==(const  Vector& other);
+    bool operator!=(const  Vector& other);
+    bool operator< (const  Vector& other);
+    bool operator>(const   Vector& other);
+    bool operator<= (const Vector& other);
+    bool operator>=(const  Vector& other);
+
+    friend ostream& operator<<(ostream& os, Vector& v);
+};
+
