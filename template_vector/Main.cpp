ca80609d46e553dafe3b4aa673e840d4b372e320
LoraHauret <117381884+LoraHauret@users.noreply.github.com>
2024-11-20 20:05:42 +0200

    template vector homework

---
+++ template_vector/Main.cpp
@@ -0,0 +1,192 @@
+﻿#pragma once
+#include "vector.h"
+
+int main()
+{
+    setlocale(0, "");
+    srand(time(0));
+   Vector<unsigned int, int> ar;
+   cout << "\nвставка элемента в пустой массив\n";
+   bool isInserted = ar.Insert(0, 1);   
+    ar.Print();
+    cout << "\nпопытка вставки с неверным индексом:\n";
+    isInserted = ar.Insert(100, 2);
+    if(isInserted == false)
+	cout << "элемент не вставлен\n";
+    cout << ar;
+
+    isInserted = ar.Insert(2, 2);
+    if (isInserted == false)
+      cout << "элемент не вставлен\n";
+    cout << ar;
+
+    //isInserted = ar.Insert(1, 2);
+    // isInserted = ar.Insert(1, 3);
+    cout << "\nвставка 2х элементов в конец массива:\n";
+    isInserted = ar.Insert(1, 4, 2); // вставка 2х элементов в конец массива
+    cout << ar;
+    cout << "\nвставка 1 элемента в середину массива по индексу:\n";
+    isInserted = ar.Insert(1, 3); // вставка 1 элемента в середину массива по индексу
+    cout << ar;    
+    cout << "\nвставка 10 элементов в середину массива для проверки пересчета capacity:\n";
+    isInserted = ar.Insert(1, 2, 10); // вставка 10 элементов в середину массива для проверки пересчета capacity
+    cout << ar;
+    // removeat
+    cout << "\nУдаление от 2го элемента 9ти элементов:\n";
+    ar.RemoveAt(2, 9);
+    cout << ar;
+    cout << "\nУдаление 4го элемента:\n";
+    ar.RemoveAt(4);
+    cout << ar;
+   
+    // добавляю в массив дополнительные элементы
+    cout << "\nдобавляю в массив дополнительные элементы\n";
+    ar.Insert(3, 5, 2);
+    ar.PushBack(5);
+    ar.PushFront(5);
+    cout << ar;
+    cout << "\nудаляю из массива все вхождения 5ки\n";
+    ar.RemoveByValue(5, 1);
+    cout << ar;
+
+    // удаление первого элемента
+    cout << "\nудаление первого элемента\n";
+    ar.PopFront();
+    ar.Print();
+    // удаление последнего элемента
+    cout << "\nудаление последнего элемента\n";
+    ar.PopBack();
+    cout << ar;
+
+    // "очищение" массива
+    cout << "\nдля удобства очищаю и заполняю массив последовательно расположенными числами\n";
+    ar.Clear();
+    ar.PushBack(1);
+    ar.PushBack(2);
+    ar.PushBack(3);
+    ar.PushBack(4);
+    ar.PushBack(5);
+    ar.PushBack(6);
+    ar.PushBack(4);
+    ar.PushBack(8);
+    cout << ar;
+
+    // Нахожу индекс 4ки
+    cout << "\nНахожу индекс 4ки\n";
+    cout << ar.IndexOf(4) << "\n";
+    cout << "\nНахожу индекс последнего вхождения 4ки\n";
+    cout << ar.LastIndexOf(4) << "\n";
+    cout << "\nНахожу индекс несуществующего элемента\n";
+    cout << ar.IndexOf(14) << "\n";
+    cout << "\nРеверсирую массив\n";
+    ar.Reverse();
+    cout << ar;
+    cout << "\nСортирую по возрастанию\n";
+    ar.SortAsc();
+    cout << ar;
+    cout << "\nСортирую по убыванию\n";
+    ar.SortDesc();
+    cout << ar;
+    cout << "\nПеремешиваю значения массива\n";
+    ar.Shuffle();
+    cout << ar;
+    cout << "\nЗаполняю массив случайными значениями\n";
+    ar.RandomFill();
+    cout << ar;
+    cout << "\nделаю трим массиву\nРазмер до прически:\n";
+    cout <<"size: "<< ar.GetSize()<<" capacity: "<<ar.GetCapacity() << "\n\n";
+    ar.TrimToSize();
+    cout << "Размер после укладки:\n";
+    cout << "size: " << ar.GetSize() << " capacity: " << ar.GetCapacity() << "\n\n";
+    cout << "Сам массив:\n";
+    cout << ar;
+    cout << "\nполучение значения элемента по индексу\n";
+    ar.SortAsc();
+    cout << ar;
+    try
+    {
+	cout <<"Элемент по индексу 5: " << ar.GetElementAt(5) << "\n";
+	//cout << "Элемент по индексу -1: ";
+	//cout<< ar.GetElementAt(-1) << "\n";
+	cout << "Элемент по индексу 100: ";
+	cout << ar.GetElementAt(100) << "\n";
+    }
+    catch (exception ex)
+    {
+	cout << ex.what() << "\n";
+    }
+    cout << "\nсоздание еще одного вектора2 с таким же размером, как вектор 1\n"; 
+    Vector<unsigned int, int> ar2;
+    for(int i = 0; i<ar.GetSize(); i++)
+	ar2.PushBack(i);
+    cout << "массив #2:\n";
+    cout << ar2;
+    cout << "\nпроверка на равенство: \n";
+    cout << "массив ar equals ar2: " << boolalpha << (ar.Equals(ar2)) << endl;
+    cout << "массив ar == ar2: " << boolalpha << (ar==ar2) << endl;
+    cout << "массив ar != ar2: " << boolalpha << (ar != ar2) << endl;
+    cout << "массив ar > ar2: " << boolalpha << (ar > ar2) << endl;
+    cout << "массив ar >= ar2: " << boolalpha << (ar >= ar2) << endl;
+    cout << "массив ar < ar2: " << boolalpha << (ar < ar2) << endl;
+    cout << "массив ar <= ar2: " << boolalpha << (ar <= ar2) << endl;
+
+    cout << "\nсоздание копии вектора 1\n";
+    Vector<unsigned int, int>* ar3 = ar.Clone();
+    cout << "вектор 1: " << ar;
+    cout << "вектор 3: " << *ar3;
+    cout << "\nИзменение вектора 1  (popback)\n";
+    ar.PopBack();
+    cout << "вектор 1: " << ar;
+    cout << "вектор 3: " << *ar3;
+    cout << "\nИзменение вектора 3  (popfront)\n";
+    ar3->PopFront();
+    cout << "вектор 1: " << ar;
+    cout << "вектор 3: " << *ar3;
+
+    delete ar3;
+    cout << "\nсоздание вектора при помощи конструктора копирования.\n";
+    Vector<unsigned int, int> ar4(ar);
+    cout << "вектор 1: " << ar;
+    cout << "вектор 4: " << ar4;
+    cout << "\nИзменение вектора 1  (popback)\n";
+    ar.PopBack();
+    cout << "вектор 1: " << ar;
+    cout << "вектор 4: " << ar4;
+    cout << "\nИзменение вектора 4  (popfront)\n";
+    ar4.PopFront();
+    cout << "вектор 1: " << ar;
+    cout << "вектор 4: " << ar4;
+
+    // cout << "\n\n";
+    cout << "\nсоздаю вектор 6 при помощи пустого вектора 5 для проверки конструктора копирования\n";
+    Vector<unsigned int, int> ar5;
+    Vector<unsigned int, int> ar6(ar5);
+    cout << ar5;
+    cout << ar6;
+    cout << "\nприсваивание значений ar4 значениям ar5\n"; 
+    ar5 = ar4;
+    cout << "\nИзменение вектора 4  (popback)\n";
+    ar4.PopBack();
+    cout << "вектор 4: " << ar4;
+    cout << "вектор 5: " << ar5;
+    cout << "\nИзменение вектора 5  (popfront)\n";
+    ar5.PopFront();
+    cout << "вектор 4: " << ar4;
+    cout << "вектор 5: " << ar5;
+
+    cout << "\nдоступ к вектору через перегруженные скобки\n";
+    for (int i = 0; i < ar5.GetSize(); i++)
+    {
+	cout << ar5[i] << " ";
+	ar5[i] = i;
+    }	
+    cout << "\n";
+
+    for (int i = 0; i < ar5.GetSize(); i++)
+	cout << ar5[i] << " ";
+    cout << "\n";
+    cout << "\nВвод значений при помощи перегруженного оператора >>\n";
+    cin >> ar6;
+    cout << ar6;
+    return 1;
+}
---
+++ template_vector/vector.h
@@ -0,0 +1,596 @@
+﻿#pragma once
+#include <iostream>
+#include <random>
+using namespace std;
+template <typename T, typename K>
+class Vector // Vector
+{
+    T size = 0; // количество действительно присутствующих элементов в контейнере
+    T capacity = 10; // ёмкость (вместительность, запас памяти)
+    K* data = nullptr; // указатель на динамический массив целых чисел
+
+    void IncreaseCapacity();
+    void IncreaseCapacity(T);
+    void EnsureCapacity();
+    void EnsureCapacity(T);
+public:
+    Vector() : Vector(10)
+    {
+	// cout << "C-TOR WITHOUT PARAMS!\n";
+    }
+
+    Vector(T capacity)
+    {
+	if (capacity < 10)
+	{
+	    capacity = 10;
+	}
+	this->capacity = capacity;
+	data = new K[capacity];
+	// cout << "C-TOR WITH PARAMS!\n";
+	cout << "\n\n ********* КОНСТРУКТОР ДЛЯ " << this << " **********\n\n";
+    }
+
+    ~Vector()
+    {
+	cout << "\n\n ********* ДЕСТРУКТОР ДЛЯ " << this << " **********\n\n";	
+	// cout << "DESTRUCTOR!\n";
+	if (data != nullptr)
+	{
+	    delete[] data;
+	    data = nullptr;
+	}
+    }
+
+    void PushBack(K value)
+    {	
+	EnsureCapacity(); // проверка, хватит ли места для нового элемента
+	data[size] = value;
+	size++;
+    }
+
+    void PushFront(K value)
+    {
+	EnsureCapacity();
+	for (K i = size; i > 0; i--) // i = 1
+	{
+	    data[i] = data[i - 1]; // data[1] = data[0]
+	}
+	data[0] = value;
+	size++;
+    }
+
+    void Clear()
+    {
+	size = 0;
+    }
+
+    bool IsEmpty() const
+    {
+	return size == 0;
+    }
+
+    void Print() const
+    {
+	if (IsEmpty())
+	{
+	    cout << "Vector is empty.\n";
+	    return;
+	}
+
+	for (K i = 0; i < size; i++)
+	{
+	    cout << data[i] << " ";
+	}
+	cout << "\n";
+    }
+
+    // ...остальные методы...
+    bool Insert(T index, K value, T countinsert = 1);
+    bool RemoveAt(T index, T countdelete = 1);
+    bool RemoveByValue(K value, bool remOne = true);
+    bool PopFront();
+    bool PopBack();
+    bool TrimToSize();
+    int IndexOf(K value);
+    int LastIndexOf(K value);
+    bool Reverse();
+    bool SortAsc(); 
+    bool SortDesc();
+    bool Shuffle();
+    bool RandomFill();
+    bool Equals(const Vector<T,K>& other);
+    int GetElementAt(T);
+    Vector<T, K>*& Clone();
+    Vector<T, K>(const Vector<T, K>&);
+    Vector<T, K>& operator=(const Vector<T, K>&);
+ ////  /* void testCopyConstructor(Vector v)
+ ////   {	v.Print();
+	////cout << "testing" << endl;
+ ////   }*/
+    K& operator[](T index);
+    T GetSize()
+    {
+	return size;
+    }
+    T GetCapacity()
+    {
+	return capacity;
+    }
+    bool operator==(const  Vector<T, K>& other);
+    bool operator!=(const  Vector<T, K>& other);
+    bool operator< (const  Vector<T, K>& other);
+    bool operator>(const   Vector<T, K>& other);
+    bool operator<= (const Vector<T, K>& other);
+    bool operator>=(const  Vector<T, K>& other);
+
+    template <typename T, typename K>
+    friend ostream& operator<<(ostream& os, Vector<T, K>& v);
+    template <typename T, typename K>
+    friend istream& operator>>(istream& is, Vector<T, K>& v);
+};
+
+
+template <typename T, typename K>
+ostream& operator<<(ostream& os, Vector<T, K>& v)
+{
+    if (v.IsEmpty())
+    {
+        os << "Vector is empty.\n";
+        return os;
+    }
+
+    for (int i = 0; i < v.size; i++)
+    {
+        os << v.data[i] << " ";
+    }
+    os << "\n";
+
+    return os;
+}
+
+template <typename T, typename K>
+istream& operator>>(istream& is, Vector<T, K>& v) {
+    unsigned int count;
+    cout << "Введите количество элементов вектора: ";
+    is >> count;
+    v.EnsureCapacity();
+
+    int val;
+    try
+    {
+        for (int i = 0; i < count; i++)
+        {
+            cout << "Введите элемент # " << i + 1 << ": ";
+            is >> val;
+            v.PushBack(val);
+        }
+
+    }
+    catch (exception ex)
+    {
+        cout << "данные не введены\n Причина: ";
+        cout << ex.what() << "\n";
+    }
+    return is;
+
+}
+
+
+
+// Insert (вставка одного элемента в массив по указанному индексу, с проверкой на выход за пределы массива)
+template <typename T, typename K>
+bool Vector<T, K>::Insert(T index, K value, T countinsert)//куда вставлять, что вставлять, сколько раз
+{
+    //cout << "\ninside Insert\n";
+    if (index < 0 || index > size || countinsert == 0)
+        return false;
+
+    if (index == size)                        // если индекс вставки == количесву элементов, то вставляю в конец массива
+    {
+        for(T i = 0; i < countinsert; i++)
+            PushBack(value);
+        return true;
+    }
+        
+
+    EnsureCapacity(countinsert);
+    for (K i = size + countinsert-1; i > size + countinsert - 1 - (size - index); i--)
+        data[i] = data[i - countinsert];
+
+    size += countinsert;
+
+    while (countinsert)
+    {
+        data[index] = value;
+        --countinsert;
+        ++index;
+    }
+       
+    
+    //cout << *this;
+    return true;
+}
+
+//// (удаление одного элемента по указанному индексу. Должна быть проверка на допустимость индекса)
+template <typename T, typename K>
+bool Vector<T, K>::RemoveAt(T index, T countdelete)
+{
+    if (IsEmpty())
+        return false;
+
+    if (index < 0 || index >= size || countdelete == 0)
+        return false;
+
+    if (countdelete >= size - index)   // если пользователь хочет удалить больше элементов, нежели находится правее от указанного индекса, то просто "удаляю" все элементы до конца массива
+    {
+        size = index;
+        return true;
+    }
+
+    for (int i = index + countdelete; i < size; i++)
+    {
+        data[index] = data[i];
+        ++index;
+    }
+
+    size -= countdelete;
+
+    return true;
+}
+
+//// RemoveByValue (удаление одного или всех элементов, значение которых совпадает со значением переданного параметра)
+template <typename T, typename K>
+bool Vector<T, K>::RemoveByValue(K value, bool remAll)
+{
+    if (IsEmpty())
+        return false;
+    for (T i = 0; i < size; i++)
+    {
+        if (data[i] == value)
+        {
+            RemoveAt(i);
+            --i; // поскольку элемент удален 
+            if (remAll)
+                continue;
+            else
+                return true;
+        }
+
+    }
+    return false; // элемент не найден
+}
+
+////  (удаление элемента по индексу 0 из массива)
+template <typename T, typename K>
+bool Vector<T, K>::PopFront()
+{
+    return RemoveAt(0);
+}
+
+//  (удаление последнего элемента из массива)
+template <typename T, typename K>
+bool Vector<T, K>::PopBack()
+{
+    return RemoveAt(size - 1);
+}
+
+//(метод подгоняет значение capacity под size, естественно с перевыделением памяти)
+template <typename T, typename K>
+bool Vector<T, K>::TrimToSize()
+{
+    if (IsEmpty() || size == capacity)
+        return false;
+
+    K* temp = new K[size];
+    for (T i = 0; i < size; i++)
+    {
+        temp[i] = data[i];
+    }
+    delete[] data;
+    data = temp;
+    capacity = size;
+    return true;
+}
+
+
+//  (линейный поиск слева направо первого вхождения в массив указанного значения. В результате работы вернуть индекс найденного элемента, а eсли ничего не найдено, вернуть -1)
+template <typename T, typename K>
+int Vector<T, K>::IndexOf(K value)
+{
+    if (IsEmpty())
+        return -1;
+    for (T i = 0; i < size; i++)
+    {
+        if (data[i] == value)
+            return i;
+    }
+    return -1;
+}
+
+//LastIndexOf(линейный поиск справа налево вхождения в массив указанного значения.В результате работы вернуть индекс найденного элемента, а eсли ничего не найдено, вернуть - 1)
+template <typename T, typename K>
+int Vector<T, K>::LastIndexOf(K value)
+{
+    if (IsEmpty())
+        return -1;
+
+    for (T i = size - 1; i >= 0; i--)
+    {
+        if (data[i] == value)
+            return i;
+    }
+    return -1;
+}
+
+
+//  (изменение порядка следования элементов в массиве на противоположный)
+template <typename T, typename K>
+bool Vector<T, K>::Reverse()
+{
+    if (IsEmpty())
+        return false;
+    for (T i = 0, j = size - 1; i < size / 2; i++, j--)
+    {
+        K temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+    }
+    return true;
+}
+
+//SortAsc (быстрая сортировка значений элементов массива по возрастанию)
+template <typename T, typename K>
+bool Vector<T, K>::SortAsc()
+{
+    if (IsEmpty())
+        return false;
+
+    for (T i = size - 1; i > 0; i--)
+    {
+        for (T j = 0; j < i; j++)
+        {
+            if (data[j] > data[j + 1])
+            {
+                K temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+            }
+        }
+    }
+    return true;
+}
+
+//  SortDesc (быстрая сортировка значений элементов массива по убыванию)
+template <typename T, typename K>
+bool Vector<T, K>::SortDesc()
+{
+    if (IsEmpty())
+        return false;
+
+    for (T i = size - 1; i > 0; i--)
+    {
+        for (T j = 0; j < i; j++)
+        {
+            if (data[j] < data[j + 1])
+            {
+                K temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+            }
+        }
+    }
+    return true;
+}
+
+template <typename T, typename K>
+bool Vector<T, K>::Shuffle()
+{
+    if (IsEmpty())
+        return false;
+    for (T i = 0; i < 100; i++)
+    {
+        T ind1 = rand() % size;
+        T ind2 = rand() % size;
+        if (ind1 == ind2)
+            continue;
+        //cout << ind1 << " " << ind2 << endl;
+        K temp = data[ind1];
+        data[ind1] = data[ind2];
+        data[ind2] = temp;
+    }
+    return true;
+}
+
+// (заполнение массива случайными значениями)
+template <typename T, typename K>
+bool Vector<T, K>::RandomFill()
+{
+    if (!IsEmpty())
+    {
+        for (T i = 0; i < size; i++)
+        {
+            data[i] = rand() % 100;
+        }
+        return true;
+    }
+    return false;
+}
+
+//  (в качестве параметра передаётся константную ссылку на другой вектор. Метод сравнивает массивы не только по количеству элементов, но и по их содержимому)
+template <typename T, typename K>
+bool Vector<T, K>::Equals(const Vector<T, K>& other)
+{
+    return (*this == other);
+}
+
+// GetElementAt (возврат копии элемента массива по указанному индексу, с проверкой на выход за пределы массива)
+template <typename T, typename K>
+int Vector<T, K>::GetElementAt(T index)
+{
+    if (IsEmpty())
+        throw ("вектор пуст!");
+    if (index < 0 || index >= size)
+        throw out_of_range("индекс за пределами допустимых значений!");
+    return data[index];
+}
+
+//  (метод создаёт точную копию вектора).
+template <typename T, typename K>
+Vector<T, K>*& Vector<T, K>::Clone()
+{
+    Vector* ret = new Vector(size);
+    if (!IsEmpty())
+    {
+        for (T i = 0; i < size; i++)
+        {
+            ret->PushBack(data[i]);
+        }
+    }
+    return ret;
+}
+template <typename T, typename K>
+Vector<T, K>::Vector(const Vector<T, K>& other)
+{
+    cout << "\n\n ********* КОНСТРУКТОР  КОПИРОВАНИЯ ДЛЯ " << this << " **********\n\n";
+
+
+    if (other.size != 0)
+    {
+        this->capacity = other.capacity;
+        this->size = other.size;
+        data = new int[capacity];
+        //копирую один блок памяти в другой
+        // memcpy(data, other.data, sizeof(int) * size);
+        // или вот так
+        for (T i = 0; i < size; i++)
+            data[i] = other.data[i];
+    }
+    else
+    {
+        //Vector();
+        if (capacity < 10)
+        {
+            capacity = 10;
+        }
+        this->capacity = capacity;
+        data = new int[capacity];
+        size = 0;
+
+    }
+}
+
+// перегрузка операции =
+template <typename T, typename K>
+Vector<T, K>& Vector<T, K>::operator=(const Vector<T, K>& other)
+{
+    if (*this != other)
+    {
+        Clear();
+        EnsureCapacity(other.size);
+
+        //копирую один блок памяти в другой
+        //for (int i = 0; i < other.size; i++)
+        //    this->PushBack(other.data[i]);        
+        // или так
+        // memcpy(data, other.data, sizeof(int) * size);
+        // или вот так
+        for (T i = 0; i < other.size; i++)
+            data[i] = other.data[i];
+
+        this->size = other.size;
+    }
+
+    return *this;    //возвращаю объект, чтобы можно было присваивать векторы друг другу в цепочке присваивания
+}
+
+// перегрузка операции [ ]
+template <typename T, typename K>
+K& Vector<T, K>::operator[](T index)
+{
+    if (IsEmpty())
+        throw ("вектор пуст!");
+    if (index < 0 || index >= size)
+        throw out_of_range("индекс за пределами допустимых значений!");
+    return data[index];
+}
+
+
+//// перегрузка операции ==
+template <typename T, typename K>
+bool Vector<T, K>::operator==(const Vector<T, K>& other)
+{
+    if (size != other.size)
+        return false;
+    for (T i = 0; i < size; i++)
+    {
+        if (data[i] != other.data[i])
+            return false;
+    }
+    return true;
+}
+template <typename T, typename K>
+bool Vector<T, K>::operator!=(const Vector<T, K>& other)
+{
+    return !(*this == other);
+}
+template <typename T, typename K>
+bool Vector<T, K>::operator< (const  Vector<T, K>& other)
+{
+    return size < other.size;
+}
+template <typename T, typename K>
+bool Vector<T, K>::operator>(const Vector<T, K>& other)
+{
+    return size > other.size;
+}
+template <typename T, typename K>
+bool Vector<T, K>::operator<= (const Vector<T, K>& other)
+{
+    return size <= other.size;
+}
+template <typename T, typename K>
+bool Vector<T, K>::operator>=(const  Vector<T, K>& other)
+{
+    return size >= other.size;
+}
+
+template <typename T, typename K>
+void Vector<T, K>::IncreaseCapacity()
+{
+    T temp_capacity = size + 10;
+    K* temp = new K[temp_capacity];
+    for (K i = 0; i < size; i++)
+        temp[i] = data[i];
+    delete data;
+    data = temp;
+    capacity = temp_capacity;
+}
+template <typename T, typename K>
+void Vector<T, K>::IncreaseCapacity(T min_incr)
+{
+    T temp_capacity = size + min_incr + 10;
+    K* temp = new K[temp_capacity];
+    for (K i = 0; i < size; i++)
+        temp[i] = data[i];
+    delete[] data;
+    data = temp;
+    capacity = temp_capacity;
+}
+template <typename T, typename K>
+void Vector<T, K>::EnsureCapacity()
+{
+    /*if (data == nullptr)
+        data = new int[10];*/
+
+    if (size == capacity)
+        IncreaseCapacity();
+}
+template <typename T, typename K>
+void Vector<T, K>::EnsureCapacity(T incr)
+{
+    /*if (data == nullptr)
+        data = new int[10];*/
+    if (size + incr >= capacity)
+        IncreaseCapacity(incr);
+}
